12c45f4523e2c4becc7daea6bdd7045a03bba523
Scott McKenzie <slammayjammay@gmail.com>
2015-06-11 13:54:22 -0700

    Create category index item for home page

--- app/assets/javascripts/views/home/category_index_item.js
+++ app/assets/javascripts/views/home/category_index_item.js
@@ -1,13 +1,13 @@
-YelpClone.Views.HomeIndex = Backbone.View.extend({
-  template: JST['home/category_index_item'],
-
-  initialize: function () {
-    this.listenTo(this.collection, "sync", this.render);
-  },
-
-  render: function () {
-    var content = this.template({ category: this.model =});
-    this.$el.html(content);
-    return this;
-  }
-});
+// YelpClone.Views.HomeIndex = Backbone.View.extend({
+//   template: JST['home/category_index_item'],
+//
+//   initialize: function () {
+//     this.listenTo(this.collection, "sync", this.render);
+//   },
+//
+//   render: function () {
+//     var content = this.template({ category: this.model =});
+//     this.$el.html(content);
+//     return this;
+//   }
+// });
