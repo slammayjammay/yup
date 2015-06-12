7ab7fd25d72cb281255d977fee4a2f5a5b3396ce
Scott McKenzie <slammayjammay@gmail.com>
2015-06-12 16:51:59 -0700

    Add transitions to business index item views

--- app/assets/javascripts/views/businesses/business_index_item.js
+++ app/assets/javascripts/views/businesses/business_index_item.js
@@ -1,16 +1,15 @@
 YelpClone.Views.BusinessIndexItem = Backbone.View.extend({
-  className: "business-index-item",
+  className: "begin business-index-item",
   template: JST['businesses/index_item'],
+  events: {
+    "click": "redirect"
+  },
 
   initialize: function () {
     this.listenTo(this.model, "sync", this.render);
-  },
-
-  render: function () {
-    var content = this.template({ business: this.model });
-    this.$el.html(content);
-    this.displayRating();
-    return this;
+    setTimeout(function () {
+      this.$el.removeClass('begin');
+    }.bind(this), 0);
   },
 
   displayRating: function () {
@@ -18,5 +17,19 @@ YelpClone.Views.BusinessIndexItem = Backbone.View.extend({
     rating = (Math.round(rating * 2) / 2).toFixed(1);
     this.$("#input-id").rating({ disabled: true });
     this.$("#input-id").rating('update', rating);
+  },
+
+  redirect: function () {
+    Backbone.history.navigate("#businesses/" + this.model.get('id'), { trigger: true });
+  },
+
+  render: function () {
+    var content = this.template({
+      business: this.model
+    });
+
+    this.$el.html(content);
+    this.displayRating();
+    return this;
   }
 });
