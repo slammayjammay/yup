a756192e413c38719f806585a94911b8e49e14d4
Scott McKenzie <slammayjammay@gmail.com>
2015-06-16 14:44:10 -0700

    Code review

--- app/controllers/api/businesses_controller.rb
+++ app/controllers/api/businesses_controller.rb
@@ -2,9 +2,9 @@ class Api::BusinessesController < ApplicationController
   def index
     if params[:searchKeys]
       wildcard = "%#{params[:searchKeys]}%"
-      order = params[:order] || 'rating'
+      order = params[:order] || 'id'
       dir = order == 'name' ? 'ASC' : 'DESC'
-      
+
       @businesses = Business.where('name LIKE ? OR category LIKE ?', wildcard, wildcard)
       .order("#{order} #{dir}")
     else
