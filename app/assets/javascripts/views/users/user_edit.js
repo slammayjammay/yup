8fca61830fabc3b221b4ee34c05e3536a0f67de6
Scott McKenzie <slammayjammay@gmail.com>
2015-06-18 18:00:56 -0700

    Add transition to user review page

--- app/assets/javascripts/views/users/user_edit.js
+++ app/assets/javascripts/views/users/user_edit.js
@@ -1,8 +1,12 @@
 YelpClone.Views.UserEdit = Backbone.View.extend({
+  className: 'begin',
   template: JST['users/edit'],
 
   initialize: function () {
     this.listenTo(this.model, "sync", this.render);
+    setTimeout(function () {
+      this.$el.addClass('user-edit').removeClass('begin');
+    }.bind(this), 0);
   },
 
   render: function () {
