007c406b7c12885b036c4c57ebf9c17232e76c91
Scott McKenzie <slammayjammay@gmail.com>
2016-01-04 16:46:35 -0800

    Search using Yelp API

--- app/controllers/api/businesses_controller.rb
+++ app/controllers/api/businesses_controller.rb
@@ -2,21 +2,26 @@ class Api::BusinessesController < ApplicationController
   def index
     @page = params[:page] || 1
 
-    if params[:bestOf]
-      @businesses = Business.all.order('rating DESC')
-      .order('review_count DESC').page(params[:page]).per(10)
-    else
-      wildcard = "%#{params[:searchKeys].downcase}%" if params[:searchKeys]
-      order = params[:order] || 'id'
-      dir = order == 'name' ? 'ASC' : 'DESC'
-      @businesses = Business.where(
-        'name LIKE ? OR category LIKE ?',
-        wildcard,
-        wildcard
-      ).includes(:reviews).order("#{order} #{dir}").page(params[:page]).per(10)
-    end
-    render :index
+    # if params[:bestOf]
+    #   @businesses = Business.all.order('rating DESC')
+    #   .order('review_count DESC').page(params[:page]).per(10)
+    # else
+    #   wildcard = "%#{params[:searchKeys].downcase}%" if params[:searchKeys]
+    #   order = params[:order] || 'id'
+    #   dir = order == 'name' ? 'ASC' : 'DESC'
+    #   @businesses = Business.where(
+    #     'name LIKE ? OR category LIKE ?',
+    #     wildcard,
+    #     wildcard
+    #   ).includes(:reviews).order("#{order} #{dir}").page(params[:page]).per(10)
+    # end
+
+    @businesses = Yelp.client.search(
+      'San Francisco',
+      { term: params[:searchKeys] }
+    ).businesses
   end
+
 
   def show
     @business = Business.find(params[:id])
--- app/views/api/businesses/index.json.jbuilder
+++ app/views/api/businesses/index.json.jbuilder
@@ -1,7 +1,12 @@
-json.page @page
+# json.page @page
+
+# json.businesses @businesses do |business|
+#   json.extract! business, :id, :name, :rating, :review_count, :category,
+#     :address_line_1, :address_line_2, :city, :state, :image_url, :reviews,
+#     :latitude, :longitude
+# end
 
 json.businesses @businesses do |business|
-  json.extract! business, :id, :name, :rating, :review_count, :category,
-    :address_line_1, :address_line_2, :city, :state, :image_url, :reviews,
-    :latitude, :longitude
+  json.extract! business, :name, :rating, :review_count, :location, :image_url,
+    :snippet_text
 end
