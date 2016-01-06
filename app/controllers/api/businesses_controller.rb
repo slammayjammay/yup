a72b41caa23aff0c96f65c23b77dab16d31bde5a
Scott McKenzie <slammayjammay@gmail.com>
2016-01-05 19:02:15 -0800

    Remove commented out lines

--- app/controllers/api/businesses_controller.rb
+++ app/controllers/api/businesses_controller.rb
@@ -1,23 +1,9 @@
 class Api::BusinessesController < ApplicationController
   def index
     @page = params[:page].to_i || 1
-
-    # if params[:bestOf]
-    #   @businesses = Business.all.order('rating DESC')
-    #   .order('review_count DESC').page(params[:page]).per(10)
-    # else
-    #   wildcard = "%#{params[:searchKeys].downcase}%" if params[:searchKeys]
-    #   order = params[:order] || 'id'
-    #   dir = order == 'name' ? 'ASC' : 'DESC'
-    #   @businesses = Business.where(
-    #     'name LIKE ? OR category LIKE ?',
-    #     wildcard,
-    #     wildcard
-    #   ).includes(:reviews).order("#{order} #{dir}").page(params[:page]).per(10)
-    # end
+    result_limit = 5
 
     term = params[:searchKeys] || 'food'
-    result_limit = 5
     @businesses = Yelp.client.search(
       'San Francisco',
       { term: term, limit: result_limit, offset: result_limit * @page }
@@ -26,7 +12,6 @@ class Api::BusinessesController < ApplicationController
 
 
   def show
-    # @business = Business.find(params[:id])
     business_name = URLify.deaccentuate params[:id]
     @business = Yelp.client.business(business_name)
   end
