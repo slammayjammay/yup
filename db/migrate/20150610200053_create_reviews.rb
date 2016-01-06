1f7c7ca47324c25a1b246419253a190a9dc5c03c
Scott McKenzie <slammayjammay@gmail.com>
2016-01-06 14:58:14 -0800

    Save business id as string

--- db/migrate/20150610200053_create_reviews.rb
+++ db/migrate/20150610200053_create_reviews.rb
@@ -1,10 +1,10 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
-      t.float :rating, null: false
-      t.string :content, null: false
-      t.integer :business_id, null: false
-      t.integer :user_id, null: false
+      t.float   :rating,      null: false
+      t.string  :excerpt,     null: false
+      t.string  :business_id, null: false
+      t.integer :user_id,     null: false
 
       t.timestamps null: false
     end
