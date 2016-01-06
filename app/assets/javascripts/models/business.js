41ad345202e4ccf5b3d66179bfb9ded1605ec779
Scott McKenzie <slammayjammay@gmail.com>
2016-01-06 15:53:44 -0800

    Remove unnecessary methods

--- app/assets/javascripts/models/business.js
+++ app/assets/javascripts/models/business.js
@@ -1,20 +1,3 @@
 Yup.Models.Business = Backbone.Model.extend({
   urlRoot: "/api/businesses",
-
-  parse: function (response) {
-    if (response.reviews) {
-      this.reviews().set(response.reviews)
-      delete response.reviews;
-    }
-
-    return response;
-  },
-
-  reviews: function () {
-    if (!this._reviews) {
-      this._reviews = new Yup.Collections.Reviews();
-    }
-
-    return this._reviews;
-  }
 });
