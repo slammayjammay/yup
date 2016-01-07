e558b306f4f1c9702bfb477491f4e1efc4f205d0
Scott McKenzie <slammayjammay@gmail.com>
2016-01-06 17:31:52 -0800

    Remove commented out lines

--- app/assets/javascripts/views/reviews/review_index_item.js
+++ app/assets/javascripts/views/reviews/review_index_item.js
@@ -7,13 +7,7 @@ Yup.Views.ReviewIndexItem = Backbone.View.extend({
     // this.user = new Yup.Models.User({ id: this.model.get('user_id') });
     // this.user.fetch();
 
-    // this.business = new Yup.Models.Business({
-    //   id: this.model.get('business_id')
-    // });
-    // this.business.fetch();
-
     this.listenTo(this.model, "sync", this.render);
-    // this.listenTo(this.business, "sync", this.render);
     // this.listenTo(this.user, "sync", this.render);
 
     this.$el.addClass('begin');
