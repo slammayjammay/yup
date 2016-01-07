4fc1f2c2d234b5cfa46141727249ce8ea3cacd58
Scott McKenzie <slammayjammay@gmail.com>
2016-01-06 17:28:16 -0800

    Hold yelp reviews in instance variable

--- app/assets/javascripts/models/business.js
+++ app/assets/javascripts/models/business.js
@@ -1,3 +1,20 @@
 Yup.Models.Business = Backbone.Model.extend({
   urlRoot: "/api/businesses",
+
+  parse: function (response) {
+    if (response.yelp_reviews) {
+      this.yelpReviews().set(response.yelp_reviews)
+      delete response.yelp_reviews;
+    }
+
+    return response;
+  },
+
+  yelpReviews: function () {
+    if (!this._yelpReviews) {
+      this._yelpReviews = new Yup.Collections.Reviews();
+    }
+
+    return this._yelpReviews;
+  }
 });
