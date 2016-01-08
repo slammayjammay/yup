e6db4015e440b71fa638041f2de28d37c09194f0
Scott McKenzie <slammayjammay@gmail.com>
2016-01-07 17:26:30 -0800

    Set default image and format address

--- app/assets/javascripts/models/business.js
+++ app/assets/javascripts/models/business.js
@@ -1,10 +1,29 @@
 Yup.Models.Business = Backbone.Model.extend({
   urlRoot: "/api/businesses",
 
+  address: function () {
+    return this._address;
+  },
+
   parse: function (response) {
     if (response.yelp_reviews) {
       this.yelpReviews().set(response.yelp_reviews)
       delete response.yelp_reviews;
+    }
+
+    if (!response.image_url) {
+      this.set(
+        'image_url',
+        'http://i.ebayimg.com/00/s/Mzg2WDUxOA==/z/6scAAOSwiwVWTkQp/$_35.JPG'
+      );
+      delete response.image_url;
+    }
+
+    if (response.location) {
+      var street = response.location.hash.address[0];
+      var city = response.location.hash.city;
+      var country = response.location.hash.country_code;
+      this._address = street + ', ' + city + ', ' + country;
     }
 
     return response;
