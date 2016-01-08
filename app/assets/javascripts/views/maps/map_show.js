e581f687c477dc28e01592da6d514f77f679a7e3
Scott McKenzie <slammayjammay@gmail.com>
2016-01-07 17:11:38 -0800

    Refactor, remove useless code

--- app/assets/javascripts/views/maps/map_show.js
+++ app/assets/javascripts/views/maps/map_show.js
@@ -67,57 +67,14 @@ Yup.Views.MapShow = Backbone.View.extend({
     this._markers[index].setAnimation(null);
   },
 
-  initDefaultMap: function () {
-    var mapOptions = {
-      center: {
-        lat: 37.7532501,
-        lng: -122.4067001,
-      },
-      zoom: 11
-    };
+  init: function (mapOptions) {
+    if (!mapOptions) mapOptions = {};
 
     this.map = new google.maps.Map(this.el, mapOptions);
     setTimeout(function () {
       google.maps.event.trigger(this.map, 'resize');
       this.addBusinessMarkers();
     }.bind(this), 0);
-  },
-
-  initSearchMap: function () {
-    var business = this.collection.first();
-    if (!business) {
-      this.initDefaultMap();
-      return;
-    }
-    var mapOptions = {
-      center: {
-        lat: business.get('latitude'),
-        lng: business.get('longitude')
-      }
-    };
-
-    this.map = new google.maps.Map(this.el, mapOptions);
-    var bounds = new google.maps.LatLngBounds();
-
-    this.collection.each(function (business) {
-      var marker = new google.maps.Marker({
-        position: { lat: business.get('latitude'),
-                  lng: business.get('longitude') },
-        map: this.map,
-        animation: google.maps.Animation.DROP,
-        title: business.get('name')
-      });
-
-      this._markers.push(marker);
-      google.maps.event.addListener(marker, 'click', function (event) {
-        this.showInfoWindow(event, marker);
-      }.bind(this));
-
-      var bound = new google.maps.LatLng(business.get('latitude'), business.get('longitude'));
-      bounds.extend(bound);
-    }.bind(this));
-
-    this.map.fitBounds(bounds);
   },
 
   removeMarkers: function () {
