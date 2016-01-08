589aa667c59f8a59cb0a21fbd783871f4ef544eb
Scott McKenzie <slammayjammay@gmail.com>
2016-01-07 17:47:20 -0800

    Add method to use large version of image_url

--- app/assets/javascripts/models/business.js
+++ app/assets/javascripts/models/business.js
@@ -5,18 +5,27 @@ Yup.Models.Business = Backbone.Model.extend({
     return this._address;
   },
 
+  largeImageUrl: function () {
+    return this._largeImageUrl;
+  },
+
   parse: function (response) {
     if (response.yelp_reviews) {
       this.yelpReviews().set(response.yelp_reviews)
       delete response.yelp_reviews;
     }
 
-    if (!response.image_url) {
+    if (response.image_url) {
+      // Get the large version of image_url. Urls take the form of .../ms.jpg.
+      // Replace ms with o to get the original size of the image.
+      this._largeImageUrl = response.image_url.slice().replace('ms.jpg', 'o.jpg');
+
+      delete response.image_url;
+    } else {
       this.set(
         'image_url',
         'http://i.ebayimg.com/00/s/Mzg2WDUxOA==/z/6scAAOSwiwVWTkQp/$_35.JPG'
       );
-      delete response.image_url;
     }
 
     if (response.location) {
