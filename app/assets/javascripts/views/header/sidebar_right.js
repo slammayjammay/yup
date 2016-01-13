746ad983094a8de215af09640583c7b93e5f6abe
Scott McKenzie <slammayjammay@gmail.com>
2016-01-13 02:12:29 -0800

    Comment out old code

--- app/assets/javascripts/views/header/sidebar_right.js
+++ app/assets/javascripts/views/header/sidebar_right.js
@@ -1,26 +1,24 @@
 Yup.Views.SidebarRight = Backbone.CompositeView.extend({
   template: JST['header/sidebar_right'],
   id: 'sidebar-right',
-  events: {
-    'click .category-index-item': 'search'
-  },
+  // events: {
+  //   'click .category-index-item': 'search'
+  // },
 
   initialize: function (options) {
     this.query = options.query;
-    this.order = options.order;
 
-    this.listenToOnce(this.collection, 'sync', this.renderMap);
-
-    this.categories = [['restaurants', 'cutlery'], ['food', 'ice-lolly-tasted'],
-      ['nightlife', 'glass'], ['bars', 'glass'], ['shopping', 'shopping-cart'],
-      ['coffee', 'cutlery'], ['health', 'plus'], ['pets', 'home']];
-    this.renderCategories();
+    // this.categories = [['restaurants', 'cutlery'], ['food', 'ice-lolly-tasted'],
+    //   ['nightlife', 'glass'], ['bars', 'glass'], ['shopping', 'shopping-cart'],
+    //   ['coffee', 'cutlery'], ['health', 'plus'], ['pets', 'home']];
+    // this.renderCategories();
   },
 
   render: function () {
     var content = this.template({ order: this.order });
     this.$el.html(content);
     this.attachSubviews();
+    this.renderMap();
     return this;
   },
 
