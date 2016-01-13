4961c3047e25f50e9cd08c706841a42834060d8e
Scott McKenzie <slammayjammay@gmail.com>
2016-01-13 11:48:29 -0800

    Remove old code

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,44 +1,6 @@
 class Api::ReviewsController < ApplicationController
   def index
-    @reviews = Review.find_by_sql(
-      "SELECT DISTINCT
-        users.id AS user_id,
-        users.first_name,
-        users.last_name,
-        users.image_url,
-        reviews.created_at,
-        reviews.rating,
-        reviews.content,
-        reviews.business_id
-      FROM
-        (SELECT *
-        FROM followings
-        WHERE followings.follower_id = #{current_user.id}) AS sq
-      INNER JOIN reviews ON reviews.user_id = sq.followed_id
-      INNER JOIN users on reviews.user_id = users.id
-      ORDER BY reviews.created_at DESC
-      LIMIT 10;"
-    )
-
-    if @reviews.length == 0
-      @reviews = Review.find_by_sql(
-        "SELECT DISTINCT
-          users.id AS user_id,
-          users.first_name,
-          users.last_name,
-          users.image_url,
-          reviews.created_at,
-          reviews.rating,
-          reviews.content,
-          reviews.business_id
-        FROM
-          reviews
-        INNER JOIN users on reviews.user_id = users.id
-        ORDER BY reviews.created_at DESC
-        LIMIT 10;"
-      )
-    end
-    render :index
+    @reviews = Review.all
   end
 
   def create
@@ -46,13 +8,6 @@ class Api::ReviewsController < ApplicationController
     @review.user_id = current_user.id
 
     if @review.save
-      business = Business.find(@review.business_id)
-      rating = @review.rating
-
-      business.review_count += 1
-      diff = rating - business.rating
-      business.rating += diff.fdiv(business.review_count)
-      business.save
       render :show
     else
       render json: @review.errors, status: 422
@@ -66,6 +21,6 @@ class Api::ReviewsController < ApplicationController
   private
 
   def review_params
-    params.permit(:business_id, :rating, :content, :price)
+    params.permit(:business_id, :rating, :excerpt)
   end
 end
