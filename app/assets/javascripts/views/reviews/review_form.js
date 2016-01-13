c0ff4daaa1a37d56de46646c2acd3c50f4a8d368
Scott McKenzie <slammayjammay@gmail.com>
2016-01-13 11:48:08 -0800

    Fix saving, showing errors and exiting the modal

--- app/assets/javascripts/views/reviews/review_form.js
+++ app/assets/javascripts/views/reviews/review_form.js
@@ -1,5 +1,5 @@
 Yup.Views.ReviewForm = Backbone.View.extend({
-  className: "review-form",
+  className: "review-form begin",
   template: JST['reviews/form'],
   events: {
     "submit form": "save",
@@ -7,16 +7,17 @@ Yup.Views.ReviewForm = Backbone.View.extend({
   },
 
   initialize: function () {
-    this.listenTo(this.model, "sync", this.render);
     $('.backdrop').click(this.close.bind(this));
+    setTimeout(function () {
+      this.$el.removeClass('begin').addClass('end');
+    }.bind(this), 200);
   },
 
   close: function (event) {
     if ($(event.target).attr('class') === 'backdrop' ||
         $(event.target).attr('class') === 'close') {
       event.preventDefault();
-      $('.backdrop').remove();
-      this.remove();
+      this.exit();
     }
   },
 
@@ -24,24 +25,34 @@ Yup.Views.ReviewForm = Backbone.View.extend({
     this.$("#input-id").rating();
   },
 
-  parseErrors: function(model, response) {
-    var errors = response.responseJSON;
-    var $errors = this.$('ul').empty();
-    for (var prop in errors) {
-      errors[prop].forEach( function (errorMsg) {
-        var $error = $('<li>').addClass('begin');
-        $error.text(prop + " " + errorMsg);
-        $errors.append($error);
+  exit: function () {
+    this.$el.removeClass('end').addClass('begin');
+    $('.backdrop').addClass('begin');
+    $('.backdrop').one('transitionend', function () {
+      $('.backdrop').remove();
+      this.remove();
+    }.bind(this));
+  },
 
-        setTimeout(function () {
-          $error.removeClass('begin');
-        }, 0);
-      });
+  parseErrors: function(response) {
+    var $errors = this.$('ul').empty();
+    if (response.responseJSON.excerpt) {
+      var $error = $('<li class="">').text('Your review cannot be blank');
+      $errors.append($error);
     }
+
+    if (response.responseJSON.rating) {
+      var $error = $('<li class="">').text('You need to give a rating');
+      $errors.append($error);
+    }
+
+    setTimeout(function () {
+      $errors.removeClass('begin');
+    }, 0);
   },
 
   render: function () {
-    var content = this.template({ business: this.model });
+    var content = this.template();
     this.$el.html(content);
     this.$('#input-id').on('rating.change', function (e, v, c) {
       this.rating = parseFloat(v);
@@ -51,26 +62,43 @@ Yup.Views.ReviewForm = Backbone.View.extend({
     return this;
   },
 
+  renderSuccess: function () {
+    var $success = $('<div class="review-success">').text('Reviewed!');
+    $('.business-show').append($success);
+
+    setTimeout(function () {
+      $success.css('opacity', 1);
+    }, 0);
+
+    setTimeout(function () {
+      $success.css('opacity', 0);
+      $success.one('transitionend', function () {
+        $success.remove();
+      });
+    }, 3000);
+
+    this.exit();
+  },
+
   save: function (event) {
     event.preventDefault();
     var review = new Yup.Models.Review({ rating: this.rating });
-    review.set('content', this.$('textarea').val());
+    review.set('excerpt', this.$('textarea').val());
     review.set('business_id', this.model.get('id'));
 
-    var that = this;
     review.save({}, {
       success: function () {
-        that.model.fetch();
-        that.remove();
-        Backbone.history.navigate("#redirecting...");
-        Backbone.history.navigate(
-          "businesses/" + that.model.get('id'),
-          { trigger: true }
-        );
-      },
+        this.renderSuccess();
+      //   this.model.fetch();
+      //   this.remove();
+      //   Backbone.history.navigate(
+      //     "businesses/" + this.model.get('id'),
+      //     { trigger: true }
+      //   );
+      }.bind(this),
 
       error: function (model, response) {
-        this.parseErrors(model, response);
+        this.parseErrors(response);
       }.bind(this)
     });
   }
