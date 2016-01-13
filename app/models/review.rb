3ebb2c2fcb9567d0931e7942f85c3c6de6e4db32
Scott McKenzie <slammayjammay@gmail.com>
2016-01-13 11:49:25 -0800

    Remove old code

--- app/models/review.rb
+++ app/models/review.rb
@@ -14,7 +14,6 @@
 class Review < ActiveRecord::Base
   validates :user_id, :business_id, :rating, :excerpt, presence: true
   belongs_to :user
-  belongs_to :business
 
   default_scope { order('created_at DESC') }
 end
