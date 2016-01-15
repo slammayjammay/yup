cee2fa2c9897d424dd0253dbbae2b5fb3e906b5c
Scott McKenzie <slammayjammay@gmail.com>
2016-01-15 13:48:18 -0800

    Add logic to determine whether review is a yup or yelp review

--- app/assets/javascripts/models/review.js
+++ app/assets/javascripts/models/review.js
@@ -1,3 +1,18 @@
 Yup.Models.Review = Backbone.Model.extend({
-  urlRoot: '/api/reviews'
+  urlRoot: '/api/reviews',
+
+  getTimeCreated: function () {
+    if (this.isYupReview()) {
+      return new Date(this.get('created_at'));
+    } else {
+      return this.get('time_created') * 1000;
+    }
+  },
+
+  isYupReview: function () {
+    if (!this._isYupReview) {
+      this._isYupReview = this.get('created_at');
+    }
+    return this._isYupReview;
+  }
 });
