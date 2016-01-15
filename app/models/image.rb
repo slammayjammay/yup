3792ae1c6d3d8c9df09e218cc4a0bcb7b877111a
Scott McKenzie <slammayjammay@gmail.com>
2016-01-15 11:26:00 -0800

    Annotate

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: images
+#
+#  id         :integer          not null, primary key
+#  url        :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Image < ActiveRecord::Base
 end
