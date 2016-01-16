8a6fa339ca3db1786eb191fa6f2adb8480db6c6d
Scott McKenzie <slammayjammay@gmail.com>
2016-01-16 15:29:12 -0800

    Overwrite normal behavior if yelp user

--- app/assets/javascripts/views/users/user_index_item.js
+++ app/assets/javascripts/views/users/user_index_item.js
@@ -5,8 +5,14 @@ Yup.Views.UserIndexItem = Backbone.View.extend({
     "click": "redirectToUser"
   },
 
-  initialize: function () {
-    this.listenTo(this.model, "sync", this.render);
+  initialize: function (options) {
+    if (options.yelpUser) {
+      this.userName = this.model.name;
+      this.userImage = this.model.image_url
+    } else {
+      this.listenTo(this.model, "sync", this.render);
+    }
+    
     this.$el.addClass('user-index-item');
     setTimeout(function () {
       this.$el.removeClass('begin');
@@ -18,7 +24,10 @@ Yup.Views.UserIndexItem = Backbone.View.extend({
   },
 
   render: function () {
-    var content = this.template({ user: this.model });
+    var content = this.template({
+      userName: this.userName || this.model.get('name'),
+      userImage: this.userImage || this.model.get('image_url')
+    });
     this.$el.html(content);
     return this;
   }
