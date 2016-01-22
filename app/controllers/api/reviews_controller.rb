9604ce182500b3bc98ee804160b5e9006776029b
Scott McKenzie <slammayjammay@gmail.com>
2016-01-21 18:12:50 -0800

    Fix integer error

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -17,7 +17,7 @@ class Api::ReviewsController < ApplicationController
   def sample
     limit = params[:limit] || 3
     sample_businesses = Yelp.client.search(
-      'San Francisco', { limit: limit, offset: 3 * rand(10) }
+      'San Francisco', { limit: limit.to_i, offset: 3 * rand(10) }
     )
 
     @sample_reviews = []
